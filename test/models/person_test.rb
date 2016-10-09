1785edcfeadc72c23dcedc36a4344aae652b90a8
Harvin Llanes <harvin.llanes@gmail.com>
2016-10-09 13:47:55 -0600

    start minitest

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -1,7 +1,16 @@
-require 'test_helper'
 
-class PersonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+require 'minitest/autorun'
+require './app/models/person'
+
+
+class PersonTest < Minitest::Test
+ def valid_params
+ 	{ first_name: "John", last_name: "Doe", email: "john.doe@example.com",job: "Developer", bio: "Loren Ipsum Dolor", gender: "Male", birthdate: "01/10/1900",picture: 'https://tracker.moodle.org/secure/attachment/30912/f3.png'}
+ end
+ 
+ def test_exist_first_name
+    params = valid_params.clone
+    params.delete :first_name    
+    person = Person.new params   
+ end
 end
