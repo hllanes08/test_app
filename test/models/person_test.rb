ff6b6d32ae198875cd7f9fa73b1ece361168a6f7
Harvin Llanes <harvin.llanes@gmail.com>
2016-10-09 14:03:39 -0600

    Starting test

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -1,9 +1,7 @@
+require 'test_helper'
 
-require 'minitest/autorun'
-require './app/models/person'
-
-
-class PersonTest < Minitest::Test
+class PersonTest < ActiveSupport::TestCase 
+       
  def valid_params
  	{ first_name: "John", last_name: "Doe", email: "john.doe@example.com",job: "Developer", bio: "Loren Ipsum Dolor", gender: "Male", birthdate: "01/10/1900",picture: 'https://tracker.moodle.org/secure/attachment/30912/f3.png'}
  end
