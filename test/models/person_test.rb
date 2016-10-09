e766ef365dfd50b9dea49ee8e2591d22a678c026
Harvin LLanes <harvin.llanes@gmail.com>
2016-10-09 15:59:39 -0600

    Validate email

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -30,8 +30,22 @@ class PersonTest < ActiveSupport::TestCase
     assert_includes(@person.errors[:email],"can't be blank")
  end
  test "email should be well formmated"  do
+    @person.email = "lorenipsumdolor"
     @person.valid?
-    assert_includes(@person.errors[:email],"can't be blank")
+    assert_includes(@person.errors[:email],"is invalid")
+ end
+ 
+ test "email should be unique"  do
+    Person.create(first_name: "Johm", last_name: "Doe",email: "john.doe@mail.com", job: "developer", birthdate: "01/01/2000")
+    @person.email = "john.doe@mail.com"
+    @person.valid?
+    assert_includes(@person.errors[:email],"has already been taken")
+ end
+
+ test "email must be minimim 75 characters"  do 
+    @person.email = "Loren ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat Loren ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+    @person.valid?
+    assert_includes(@person.errors[:email],"is too long (maximum is 254 characters)")
  end
 
 
